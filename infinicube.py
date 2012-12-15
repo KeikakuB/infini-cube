5d2de30af337f5251b0bda4b8b6ff391cacffa7b
Bill Tyros <tyros.bill@gmail.com>
2012-12-15 18:48:15 -0500

    create different classes for different cubes

--- infinicube.py
+++ infinicube.py
@@ -21,7 +21,7 @@ import logging
 import random
 import configparser
 
-from thecubes import Cube
+from thecubes import *
 
 
 def main():
@@ -29,8 +29,8 @@ def main():
     def play_loss_sound():
         pygame.mixer.music.stop()
         
-        loss_tone = pygame.mixer.Sound(sound_folder + settings['sound']['loss_tone'])
-        loss_tone.set_volume(float(settings['sound']['volume']))
+        loss_tone = pygame.mixer.Sound(sound_folder + settings['sound']['LossTone'])
+        loss_tone.set_volume(float(settings['sound']['Volume']))
         loss_tone.play()
         
         pygame.time.wait(int(loss_tone.get_length() * 1000))
@@ -43,34 +43,29 @@ def main():
     settings = configparser.ConfigParser()
     settings.read('config' + os.sep + 'settings.ini')
     
-    image_folder = settings['images']['folder_name'] + os.sep
-    sound_folder = settings['sound']['folder_name'] + os.sep
+    sound_folder = settings['sound']['FolderName'] + os.sep
 
     pygame.init()
     
-    pygame.display.set_caption("InfiniCube v0.3")
+    pygame.display.set_caption("InfiniCube v0.4")
     
     font = pygame.font.SysFont("comicsansms", 12)
     
-    size = width, height = (int(settings['graphics']['width']), int(settings['graphics']['height'])) 
+    size = width, height = (int(settings['graphics']['Width']), int(settings['graphics']['Height'])) 
     speed = [0, 0]
     black = (0, 0, 0)
-    frame_rate = int(settings['graphics']['frame_rate'])
+    frame_rate = int(settings['gameplay']['FrameRate'])
     
-    pygame.mixer.music.load(sound_folder + settings['sound']['theme'])
-    pygame.mixer.music.set_volume(float(settings['sound']['volume']))
+    pygame.mixer.music.load(sound_folder + settings['sound']['Theme'])
+    pygame.mixer.music.set_volume(float(settings['sound']['Volume']))
     
     pygame.mixer.music.play(loops=-1)
     
     screen = pygame.display.set_mode(size)
     
-    good_cube = Cube(image_folder + settings['images']['good_cube'])
-    
-    #Move good cube to middle of screen
-    good_cube.rect = good_cube.rect.move(width//2, height//2)
+    good_cube = PlayerCube()
     
     game_clock = pygame.time.Clock()
-    
     
     elapsed_time = 0
     
@@ -82,8 +77,6 @@ def main():
     current_bad_cube_speed = base_bad_cube_speed
     
     bad_cube_counter = 0
-    
-    spawn_buffer = 15
     while True:       
         bad_cube_counter += 1
         
@@ -91,30 +84,16 @@ def main():
             score_timer = font.render(str(int(elapsed_time / 1000)), True, (255, 255, 255))
         
         #Creates bad cubes
-        if bad_cube_counter % seconds_to_frames(frame_rate, 0.3) == 0:
-            bad_cube = Cube(image_folder + settings['images']['bad_cube'] )
+        if bad_cube_counter % seconds_to_frames(frame_rate, float(settings['gameplay']['SpawnRate'])) == 0:
+            cube_type = random.randint(0, 2)
             
-            spawn_zone = random.randint(0, 3)
+            if cube_type == 0:
+                bad_cube = HoriCube(current_bad_cube_speed)
+            elif cube_type == 1:
+                bad_cube = VertiCube(current_bad_cube_speed)
+            elif cube_type == 2:
+                bad_cube = DiaCube(current_bad_cube_speed) 
             
-            #Left-Right
-            if spawn_zone == 0 or spawn_zone == 1:
-                if spawn_zone == 0:
-                    bad_cube.rect = bad_cube.rect.move(spawn_buffer, random.randint(spawn_buffer, height - spawn_buffer) )
-                    new_speed = [current_bad_cube_speed, 0]
-                elif spawn_zone == 1:
-                    bad_cube.rect = bad_cube.rect.move(width - spawn_buffer, random.randint(spawn_buffer, height - spawn_buffer) )
-                    new_speed = [-current_bad_cube_speed, 0]
-            
-            #Top-Bottom
-            if spawn_zone == 2 or spawn_zone == 3:
-                if spawn_zone == 2:
-                    bad_cube.rect = bad_cube.rect.move(random.randint(spawn_buffer, width - spawn_buffer), spawn_buffer)
-                    new_speed = [0, current_bad_cube_speed]
-                elif spawn_zone == 3:
-                    bad_cube.rect = bad_cube.rect.move(random.randint(spawn_buffer, width - spawn_buffer), height - spawn_buffer)
-                    new_speed = [0, -current_bad_cube_speed]
-                    
-            bad_cube.speed = new_speed
             bad_cube_list.append(bad_cube)
         
         if bad_cube_counter % seconds_to_frames(frame_rate, 10) == 0:
@@ -123,7 +102,8 @@ def main():
         #Detects loss condition
         if len(bad_cube_list) >= 1 and good_cube.rect.collidelist( [cube.rect for cube in bad_cube_list] ) > 0:
             play_loss_sound()
-                   
+            
+            good_cube = PlayerCube()
             bad_cube_list = []
             elapsed_time = 0
             current_bad_cube_speed = base_bad_cube_speed
@@ -142,6 +122,7 @@ def main():
             if pressed_keys[pygame.K_SPACE]:
                 play_loss_sound()
                 
+                good_cube = PlayerCube()
                 bad_cube_list = []
                 elapsed_time = 0
                 current_bad_cube_speed = base_bad_cube_speed
@@ -182,13 +163,13 @@ def main():
         good_cube.rect = good_cube.rect.move(speed)
         
         #Keeps good cube on screen
-        good_cube.rect = keep_on_screen(good_cube.rect, width, height)
+        good_cube.keep_on_screen()
 
         screen.fill(black)
         
         for bad_cube in bad_cube_list:
-            bad_cube.rect = bad_cube.rect.move(bad_cube.speed)
-            bad_cube.rect = keep_on_screen(bad_cube.rect, width, height)
+            bad_cube.move()
+            bad_cube.keep_on_screen()
             
             screen.blit(bad_cube.surface, bad_cube.rect)
         
@@ -216,19 +197,6 @@ def move_right(speed):
 
 def move_left(speed):
     speed[0] = -4
-
-def keep_on_screen(rect, width, height):
-    #Keeps cube on screen
-    if rect.left < 0:
-        rect = rect.move(width,0)
-    elif rect.right > width:
-        rect = rect.move(-width,0)
-    elif rect.top < 0:
-        rect = rect.move(0,height)
-    elif rect.bottom > height:
-        rect = rect.move(0,-height)
-    
-    return rect
 
 if __name__== "__main__":
         main()
--- thecubes.py
+++ thecubes.py
@@ -12,15 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pygame
+import random
+import configparser
+import os
+
+settings = configparser.ConfigParser()
+settings.read('config' + os.sep + 'settings.ini')
+
+width = int(settings['graphics']['Width'])
+height = int(settings['graphics']['Height'])
+
+
+spawn_buffer = int(settings['gameplay']['SpawnBuffer'])
+
+image_folder = settings['images']['FolderName'] + os.sep
+
+player_filename = image_folder + settings['images']['PlayerCube']
+hori_filename = image_folder + settings['images']['HoriCube']
+verti_filename = image_folder + settings['images']['VertiCube']
+dia_filename = image_folder + settings['images']['DiaCube']
 
 
 class Cube(object):
-    """Represents a graphical Cube."""
-
-    def __init__(self, filename, speed=[0,0]):
+    """Represents a graphical Cube."""    
+    def __init__(self, filename, speed_x=0, speed_y=0):
         """Initializes a Cube."""
         (self._surface, self._rect) = load_image(filename)
-        self._speed = speed
+        self._speed_x = speed_x
+        self._speed_y = speed_y
     
     @property
     def surface(self):
@@ -35,12 +54,111 @@ class Cube(object):
         self._rect = new_rect
 
     @property
-    def speed(self):
-        return self._speed
+    def speed_x(self):
+        return self._speed_x
     
-    @speed.setter
-    def speed(self, x_y_list):
-        self._speed = x_y_list
+    @speed_x.setter
+    def speed_x(self, new_speed_x):
+        self._speed_x = new_speed_x
+    
+    @property
+    def speed_y(self):
+        return self._speed_y
+    
+    @speed_y.setter
+    def speed_y(self, new_speed_y):
+        self._speed_y = new_speed_y
+    
+    def move(self):
+        self._rect = self._rect.move(self._speed_x, self._speed_y)
+    
+    def keep_on_screen(self):
+        #Keeps cube on screen
+        if self.rect.left < 0:
+            self.rect = self.rect.move(width,0)
+        elif self.rect.right > width:
+            self.rect = self.rect.move(-width,0)
+        elif self.rect.top < 0:
+            self.rect = self.rect.move(0,height)
+        elif self.rect.bottom > height:
+            self.rect = self.rect.move(0,-height)
+
+class PlayerCube(Cube):
+    def __init__(self):
+        super().__init__(player_filename)
+        
+        #Move cube to middle of screen
+        self.rect = self.rect.move(width//2, height//2)
+
+class HoriCube(Cube):
+    
+    def __init__(self, speed):
+        super().__init__(hori_filename)
+        
+        if random.randint(0,1):
+            spawn_delta = get_spawn_delta('left')
+            self.speed_x = speed
+        else:
+            spawn_delta = get_spawn_delta('right')
+            self.speed_x = -speed
+        
+        self.rect = self.rect.move(spawn_delta[0], spawn_delta[1])
+
+class VertiCube(Cube):
+    
+    def __init__(self, speed):
+        super().__init__(verti_filename)
+        
+        if random.randint(0,1):
+            spawn_delta = get_spawn_delta('top')
+            self.speed_y = speed
+        else:
+            spawn_delta = get_spawn_delta('bottom')
+            self.speed_y = -speed
+        
+        self.rect = self.rect.move(spawn_delta[0], spawn_delta[1])
+            
+class DiaCube(Cube):
+    def __init__(self, speed):
+        super().__init__(dia_filename)
+        
+        if random.randint(0,1):
+            if random.randint(0,1):
+                spawn_delta = get_spawn_delta('left')
+                self.speed_x = speed
+            else:
+                spawn_delta = get_spawn_delta('right')
+                self.speed_x = -speed
+            
+            if random.randint(0,1):
+                self.speed_y = speed
+            else:
+                self.speed_y = -speed
+                
+        else:
+            if random.randint(0,1):
+                spawn_delta = get_spawn_delta('top')
+                self.speed_y = speed
+            else:
+                spawn_delta = get_spawn_delta('bottom')
+                self.speed_y = -speed
+            
+            if random.randint(0,1):
+                self.speed_x = speed
+            else:
+                self.speed_x = -speed
+        
+        self.rect = self.rect.move(spawn_delta[0], spawn_delta[1])
+
+def get_spawn_delta(direction):
+    if direction == 'left':
+        return [spawn_buffer, random.randint(spawn_buffer, height - spawn_buffer)]
+    elif direction == 'right':
+        return [width - spawn_buffer, random.randint(spawn_buffer, height - spawn_buffer)]
+    elif direction == 'top':
+        return [random.randint(spawn_buffer, width - spawn_buffer), spawn_buffer]
+    elif direction == 'bottom':
+        return [random.randint(spawn_buffer, width - spawn_buffer), height - spawn_buffer]
 
 def load_image(filename):
     image = pygame.image.load(filename).convert()
