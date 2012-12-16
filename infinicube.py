64fec70e2c4524573116f557f6a062ee1aa3ea28
Bill Tyros <tyros.bill@gmail.com>
2012-12-15 23:12:25 -0500

    increment version number

--- infinicube.py
+++ infinicube.py
@@ -57,7 +57,7 @@ def main():
     
     font = pygame.font.SysFont("comicsansms", 12)
     
-    pygame.display.set_caption("InfiniCube v0.4")
+    pygame.display.set_caption("InfiniCube v0.5")
     
     pygame.mixer.music.load(sound_folder + settings['sound']['Theme'])
     pygame.mixer.music.set_volume(float(settings['sound']['Volume']))
